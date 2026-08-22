f2e543bcc33ac4fbcdd144443762a2b65671f36a
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2026-08-22 16:23:40 +1000

    Delete the sandbox protocol, not needed, this codebase is now the proto logic and framework in one place

--- SandboxProtocol/src/SandboxProtocol.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-
-#include "ClientAPIImpl.h"
-
-extern "C" __declspec(dllexport) ClientAPI *GetClientAPI(NetBaseAPI *netbaseapi)
-{
-	static ClientAPIImpl clientapi(netbaseapi, "SandboxProtocol");
-	return &clientapi;
-}
